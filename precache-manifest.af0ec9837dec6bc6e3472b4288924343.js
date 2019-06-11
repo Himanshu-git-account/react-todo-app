328a20cda92d78325e0635ff99036b6efb6f4767
Himanshu-git-account <khimanshu0507@gmail.com>
2019-06-11 12:17:51 -0700

    Updates

---
+++ precache-manifest.af0ec9837dec6bc6e3472b4288924343.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9195f59637e8c19040f804848b877e5f",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "f752f76a3c07d5f8e14f",
+    "url": "/react-todo-app/static/css/main.cd1d5eec.chunk.css"
+  },
+  {
+    "revision": "b8e1d953b89aa454907b",
+    "url": "/react-todo-app/static/js/2.6c175dfc.chunk.js"
+  },
+  {
+    "revision": "f752f76a3c07d5f8e14f",
+    "url": "/react-todo-app/static/js/main.af3da443.chunk.js"
+  },
+  {
+    "revision": "b1aeed9f6344163e5e40",
+    "url": "/react-todo-app/static/js/runtime~main.a3a648b2.js"
+  }
+]);
